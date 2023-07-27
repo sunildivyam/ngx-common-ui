a366c258804f96a209e1e58f8fc6d53b61c96789
Sunil Kumar <sunil.divyam@gmail.com>
2023-07-27 21:55:36 +0530

    Image Browser moved to ngx-tools/fire-storage

--- image-form/public-api.ts
+++ image-form/public-api.ts
@@ -1,4 +1,3 @@
 export * from './src/components/image-form/image-form.component';
 export * from './src/interfaces/image-form.interface';
 export * from './src/image-form.module';
-export * from '../image-browser/src/classes/image-file-info.class';
