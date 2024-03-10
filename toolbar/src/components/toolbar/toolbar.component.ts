82b044cae5df3d5407664814a8134073ce4ff526
Sunil Kumar <sunil.divyam@gmail.com>
2024-03-10 16:25:33 +0530

    Toolbar Component Update | IconImage Sizes Added

--- toolbar/src/components/toolbar/toolbar.component.ts
+++ toolbar/src/components/toolbar/toolbar.component.ts
@@ -4,18 +4,23 @@ import { ToolbarItem } from '../../interfaces/toolbar.interface';
 @Component({
   selector: 'anu-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
   @Input() items: Array<ToolbarItem> = [];
   @Input() isVertical: boolean = false;
+  /**
+   * Allowed sizes of toolbar items
+   * <code>size-sm</code>, <code>size-md</code>, and <code>size-lg</code>
+   * Default size is <code>size-sm</code>
+   */
+  @Input() size: string = 'size-sm';
 
   @Output() changed = new EventEmitter<ToolbarItem>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public itemClicked(event: any, item: ToolbarItem): void {
     if (!item.href) {
